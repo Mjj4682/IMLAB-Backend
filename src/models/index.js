61be0b1ffc0aa4078dac8176dd1ffdf9d5b46ab1
walter811 <walter811@naver.com>
2022-11-01 01:09:36 +0900

    fix: database setting

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,12 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 const CouponMeta = require("./CouponMeta");
+const Coupon = require("./coupon");
+const Country = require("./country");
+const User = require("./user");
+const OrderState = require("./orderState");
+const DeliveryState = require("./deliveryState");
+const Order = require("./order");
 
 const sequelize = new Sequelize(
   config.database,
@@ -15,9 +21,27 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 
 db.CouponMeta = CouponMeta;
+db.Coupon = Coupon;
+db.Country = Country;
+db.User = User;
+db.OrderState = OrderState;
+db.DeliveryState = DeliveryState;
+db.Order = Order;
 
 CouponMeta.init(sequelize);
+Coupon.init(sequelize);
+Country.init(sequelize);
+User.init(sequelize);
+OrderState.init(sequelize);
+DeliveryState.init(sequelize);
+Order.init(sequelize);
 
 CouponMeta.associate(db);
+Coupon.associate(db);
+Country.associate(db);
+User.associate(db);
+OrderState.associate(db);
+DeliveryState.associate(db);
+Order.associate(db);
 
 module.exports = db;
