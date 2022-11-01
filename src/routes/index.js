725e46ee6f2d4ee9dd4bfe0c89f8f84872dbd05c
OolongTea620 <rlafls9596@gmail.com>
2022-11-02 06:57:19 +0900

    feat: 검색, 필터링 기능 ,  주문상태 업데이트 기능

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
 const router = express.Router();
+
 const couponRouter = require("./couponRouter.js");
+const orderRouter = require("./orderRouter");
 
 router.use("/coupon", couponRouter.router);
+router.use("/order", orderRouter.router);
 
 module.exports = router;
